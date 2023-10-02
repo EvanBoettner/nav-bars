66e5c17a240d95ccc57962752712f38163e05382
Evan Boettner <eboettner@icstars.org>
2023-10-01 22:04:50 -0500

    added sliding animation to side-bar

--- side-nav/side-nav.js
+++ side-nav/side-nav.js
@@ -1,13 +1,14 @@
 let closeBtn = document.getElementById("close-btn");
 let navContainer = document.getElementById("nav");
 let navBtn = document.getElementById("nav-btn");
+let btnContainer = document.getElementById("nav-btn-container");
 
 closeBtn.addEventListener(('click'), () => {
-    navContainer.style.display = "none";
-    navBtn.style.display = "block";
+    navContainer.classList.remove("nav-visible");
+    btnContainer.classList.remove("btn-invisible");
 });
 
 navBtn.addEventListener(('click'), () => {
-    navBtn.style.display = "none";
-    navContainer.style.display = "block";
+    btnContainer.classList.add("btn-invisible");
+    navContainer.classList.add("nav-visible")
 })
